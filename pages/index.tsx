a77ed0c7251d9173e2dba2977ba009d119217604
khaledrefaat <khaledrefaat08@gmail.com>
2022-05-22 08:42:19 +0200

    commiting things before adding sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import React, { useState } from 'react';
 import type { GetServerSideProps, GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import UnAuthorizedHome from '../components/HomePage/UnAuthorizedHome';
 import AuthorizedHome from '../components/HomePage/AuthorizedHome';
 import { getSession } from 'next-auth/react';
 import { Session } from '../typings';
+import Editor from '../components/editor/Editor';
 
 interface HomeProps {
   sess?: Session;
@@ -15,13 +17,16 @@ const Home: NextPage<HomeProps> = ({ sess }) => {
       <Head>
         <title>Medium</title>
         <meta name="description" content="Medium Blog" />
+        <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.png" />
       </Head>
       {sess ? <AuthorizedHome /> : <UnAuthorizedHome />}
+      <div className="w-96 h-96 bg-slate-100 mx-auto mb-5">
+        <Editor />
+      </div>
     </>
   );
 };
-
 export default Home;
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
